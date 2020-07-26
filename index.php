aa1396e8ff83efafee2b2964e56be14a1db1d981
pinaki123-art <62693780+pinaki123-art@users.noreply.github.com>
2020-07-26 20:14:44 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+/* Author : Pinaki
+date : 25/07/20
+*/
+echo "Hello World";
+?>
---
+++ index2.php
@@ -0,0 +1,18 @@
+<?php
+$name = 'Alex';
+$age = 21;
+if (($name)==='Alex') {
+  echo 'Welcome Alex ';
+   if ($age>=21) {
+      echo 'You\'re over 21';
+      if(1===1){
+        echo 'yes, 1 is equal to 1!';
+      }
+   } else {
+   echo 'not over 21';
+   
+   }
+} else {
+ echo 'You\'re not alex';
+}
+?>
