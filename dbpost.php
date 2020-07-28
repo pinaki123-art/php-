57b13b0e46ec7edb2df100f75680ddd833dfc3b4
pinaki123-art <62693780+pinaki123-art@users.noreply.github.com>
2020-07-28 22:58:25 +0530

    Add files via upload

---
+++ dbpost.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PostsController 
+{
+    public function show($slug)
+    {
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+
+        dd($post);
+       /*$posts = [
+           'my-first-post' => 'Hello this is my first post',
+            'my-second-post' => 'This is my second post.'
+        ];
+      */
+        if(! array_key_exists($post,$posts)){
+            abort(404, 'sorry not found');
+        }
+      
+        return view ('post', [
+            'post'=>$posts[$post]
+            ]); 
+      
+    }
+}
+?>
