ecab09a2915c28ffadfcd81cc63b7326c9b38c7b
pinaki123-art <62693780+pinaki123-art@users.noreply.github.com>
2020-08-04 22:45:39 +0530

    Create (updated)articlescontroller.php

---
+++ (updated)articlescontroller.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Article;
+
+
+use Illuminate\Http\Request;
+
+class ArticlesController extends Controller
+{
+    public function index ()
+    {
+        $articles = Article::latest()->get();
+        //dd('$articleId');
+        return view('articles.index', ['articles'=> $articles]);
+    }
+
+    public function show ($id)
+    {
+        //die("hello");
+        $article = Article::find($id);
+        
+        return view('articles.show', ['article'=> $article]);
+    }
+
+    public function create()
+    {
+        return view('articles.create');
+    }
+
+    public function store()
+    {
+        //persist the new article die('hello');
+        //dump(request()->all());
+        // security 
+        // clean up 
+
+        request()->validate([
+            'title'=> 'required',
+            'excerpt'=> 'required',
+            'body'=> 'required'
+
+        ]);
+
+        $article = new Article();
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+
+        $article->save();
+        return redirect('/articles');
+    }
+
+    
+    public function edit($id)
+    {
+        $article= Article::find($id);
+        return view('articles.edit', compact('article'));
+    }
+    
+    public function update($id)
+    {
+        request()->validate([
+            'title'=> 'required',
+            'excerpt'=> 'required',
+            'body'=> 'required'
+
+        ]);
+
+        $article = Article::find($id);
+
+        $article->title = request('title');
+        $article->excerpt = request('excerpt');
+        $article->body = request('body');
+        $article->save();
+
+        return redirect('/articles/'. $article->id);
+    }
+
+
+    /*    
+    public function store()
+    {
+
+    }
+
+
+    public function destroy()
+    {
+
+    }
+    */
+
+}
