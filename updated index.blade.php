1a7b2e2882d428605741823347f51dfe1c5e0c02
pinaki123-art <62693780+pinaki123-art@users.noreply.github.com>
2020-08-04 22:58:02 +0530

    Create updated index.blade.php

---
+++ updated index.blade.php
@@ -0,0 +1,39 @@
+@extends ('layout')
+
+    @section ('content')
+
+    <div id="wrapper">
+
+        <div 
+        id="page" 
+        class="container"
+        >
+            @forelse($articles as $article)
+            <div class="content">
+			    <div class="title">
+                    <h2>
+                        <a href="{{ $article->path() }}">
+                            {{  $article->title }}
+                        </a>
+                    </h2>
+            </div>
+
+                
+
+			<p>
+                <img 
+                src="/images/banner.jpg" 
+                alt="" 
+                class="image image-full" /> 
+            </p>
+            
+            {!! $article->excerpt !!}
+        </div>
+        @empty
+            <p> No Relevant Articles Yet </p>
+        @endforelse
+	</div>
+</div>
+
+
+@endsection
